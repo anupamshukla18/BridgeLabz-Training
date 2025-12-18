3657841172ec9ab37e9ca55a004f27cda1c74c00
anupamshukla18 <satyamshukla123kte@gmail.com>
2025-12-18 15:47:23 +0530

    [Anupam] : added First smallest number logic

---
+++ gcr-codebase/control-flow/Level-1/SmallestNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class SmallestNumber {
+	public static void main(String[] args) {
+		//Creating a Scanner object and taking user input 
+		Scanner input = new Scanner(System.in);
+		int number1 = input.nextInt();
+		int number2 = input.nextInt();
+		int number3 = input.nextInt();
+		
+		//Checking if the first number is the smallest or not and displaying the result
+		if (number1 < number2 && number1 < number3) {
+			System.out.println("Is the first number the smallest? " + "Yes");
+		} else {
+			System.out.println("Is the first number the smallest? " + "No");
+		}
+	}
+}
