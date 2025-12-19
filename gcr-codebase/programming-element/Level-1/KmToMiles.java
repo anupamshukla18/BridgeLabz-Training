2082822fd5a12dd4293b9fbd94ef8f20c1dc9182
anupamshukla18 <satyamshukla123kte@gmail.com>
2025-12-19 09:32:27 +0530

    [Anupam] : added km to miles using Scanner

---
+++ gcr-codebase/programming-element/Level-1/KmToMiles.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class KmToMiles {
+    public static void main(String[] args) {
+        //Declaring variables
+		double km;
+        Scanner input = new Scanner(System.in);
+		km = input.nextDouble();
+
+        double miles = km / 1.6;
+
+		//Displaying the result
+        System.out.println("The total miles is " + miles + " mile for the given " + km + " km");
+    }
+}
