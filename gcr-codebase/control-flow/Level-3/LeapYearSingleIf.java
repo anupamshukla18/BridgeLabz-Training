0489eb19a4f2b363d3be913bff8530d7f6d94a31
anupamshukla18 <satyamshukla123kte@gmail.com>
2025-12-20 01:06:44 +0530

    [Anupam] : added leap year using single if

---
+++ gcr-codebase/control-flow/Level-3/LeapYearSingleIf.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LeapYearSingleIf {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = input.nextInt();
+
+        // Single if condition using logical operators
+        if (year >= 1582 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
+            System.out.println(year + " is a Leap Year");
+        } 
+        else {
+            System.out.println(year + " is NOT a Leap Year or not in Gregorian Calendar");
+        }
+    }
+}
