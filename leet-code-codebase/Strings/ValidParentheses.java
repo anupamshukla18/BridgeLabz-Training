715904cecc9d5b68af2f22f7e99694e9fe7fab05
anupamshukla18 <satyamshukla123kte@gmail.com>
2025-12-28 22:37:19 +0530

    [Anupam] : added valid parentheses problem

---
+++ leet-code-codebase/Strings/ValidParentheses.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '['))
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Parentheses String: ");
+        String s = sc.nextLine();
+        System.out.println("Valid? " + isValid(s));
+    }
+}
