216bdcab05c1e5f199b15ba01df343d1194c88b5
anupamshukla18 <satyamshukla123kte@gmail.com>
2025-12-28 22:36:05 +0530

    [Anupam] : added roman to integer problem

---
+++ leet-code-codebase/Strings/RomanToInteger.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class RomanToInteger {
+
+    public static int romanToInt(String s) {
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int value = getValue(s.charAt(i));
+
+            if (i + 1 < s.length() && value < getValue(s.charAt(i + 1))) {
+                total -= value;
+            } else {
+                total += value;
+            }
+        }
+        return total;
+    }
+
+    private static int getValue(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Roman Number: ");
+        String s = sc.nextLine();
+        System.out.println("Integer Value = " + romanToInt(s));
+    }
+}
