aa329304e248c4d87bd41691870d9a86f872a7fd
anupamshukla18 <satyamshukla123kte@gmail.com>
2025-12-31 16:47:10 +0530

    [Anupam] : added developer class

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/employee/Developer.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/employee/Developer.java
@@ -1,13 +1,16 @@
 package com.inheritance.assistedproblems.employee;
 
 class Developer extends Employee {
-	public Developer(String name, int id, int salary) {
+	String programmingLanguage;
+
+	public Developer(String name, int id, int salary, String programmingLanguage) {
 		super(name, id, salary);
+		this.programmingLanguage = programmingLanguage;
 	}
 
 	// details display
 	void display() {
-		System.out.println(
-				"\nName of the Developer: " + this.name + "\nDeveloper ID: " + this.id + "\nDeveloper salary: " + this.salary);
+		System.out.println("\nName of the Developer: " + this.name + "\nDeveloper ID: " + this.id
+				+ "\nDeveloper salary: " + this.salary + "\nProgramming Language: " + this.programmingLanguage);
 	}
 }
