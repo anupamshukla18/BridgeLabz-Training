e69952df12eba758e7047279746712c71a217f14
anupamshukla18 <satyamshukla123kte@gmail.com>
2025-12-31 16:48:00 +0530

    [Anupam] : added intern class

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/employee/Intern.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/employee/Intern.java
@@ -1,13 +1,16 @@
 package com.inheritance.assistedproblems.employee;
 
 class Intern extends Employee {
-	public Intern(String name, int id, int salary) {
+	int totalTasks;
+
+	public Intern(String name, int id, int salary, int totalTasks) {
 		super(name, id, salary);
+		this.totalTasks = totalTasks;
 	}
 
 	// details display
 	void display() {
-		System.out.println(
-				"\nName of the Intern: " + this.name + "\nIntern ID: " + this.id + "\nIntern salary: " + this.salary);
+		System.out.println("\nName of the Intern: " + this.name + "\nIntern ID: " + this.id + "\nIntern salary: "
+				+ this.salary + "\nTotal tasks: " + totalTasks);
 	}
 }
