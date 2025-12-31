4073e52ebb9eaef0b7db9f582f68d06152668a2e
anupamshukla18 <satyamshukla123kte@gmail.com>
2025-12-31 16:28:10 +0530

    [Anupam] : added manager class

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/employee/Manager.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/employee/Manager.java
@@ -4,4 +4,10 @@ class Manager extends Employee {
 	public Manager(String name, int id, int salary) {
 		super(name, id, salary);
 	}
+
+	// details display
+	void display() {
+		System.out.println(
+				"\nName of the Manager: " + this.name + "\nManager ID: " + this.id + "\nManger salary: " + this.salary);
+	}
 }
