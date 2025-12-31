6a7e6e76da56e1b932075edfae9338b6dfd0cf0a
anupamshukla18 <satyamshukla123kte@gmail.com>
2025-12-31 16:27:16 +0530

    [Anupam] : added employee driver class

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/employee/EmployeeHierarchy.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/employee/EmployeeHierarchy.java
@@ -2,13 +2,16 @@ package com.inheritance.assistedproblems.employee;
 
 public class EmployeeHierarchy {
 	public static void main(String[] args) {
-		//creating employee object
-		Employee e1 = new Employee("Vishal Yadav", 100, 45000);
+		// creating employee object
+		Employee e1 = new Employee("Vishal Yadav", 100, 85000);
 		e1.display();
-		//creating manager object
+		// creating manager object
 		Manager m1 = new Manager("Anuj Dwivedi", 101, 75000);
 		m1.display();
-		//creating intern object
+		// creating employee object
+		Developer d1 = new Developer("Utkarsh Mishra", 104, 50000);
+		d1.display();
+		// creating intern object
 		Intern i1 = new Intern("Anupam Shukla", 121, 12000);
 		i1.display();
 	}
