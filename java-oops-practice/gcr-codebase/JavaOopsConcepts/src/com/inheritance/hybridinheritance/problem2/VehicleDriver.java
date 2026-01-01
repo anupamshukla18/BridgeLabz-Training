aaa70a9ad419f355b14d443f29e74d2f29ce5904
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-01 15:47:34 +0530

    [Anupam] : added vehicle driver class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/problem2/VehicleDriver.java
@@ -0,0 +1,11 @@
+package com.inheritance.hybridinheritance.problem2;
+
+public class VehicleDriver {
+	public static void main(String[] args) {
+		ElectricVehicle ev = new ElectricVehicle(150, "Tesla");
+		PetrolVehicle pv = new PetrolVehicle(180, "Honda City");
+
+		ev.charge();
+		pv.refuel();
+	}
+}
