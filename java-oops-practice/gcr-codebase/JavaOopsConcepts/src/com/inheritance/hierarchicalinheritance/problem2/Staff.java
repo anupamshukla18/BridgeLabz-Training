270ef32665638032525b2880d823e005716e84cb
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-01 15:40:24 +0530

    [Anupam] : added staff class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/problem2/Staff.java
@@ -0,0 +1,18 @@
+package com.inheritance.hierarchicalinheritance.problem2;
+
+public class Staff extends Person {
+
+	// Additional Attribute
+	private String department;
+
+	public Staff(String name, int age, String department) {
+		super(name, age);
+		this.department = department;
+	}
+
+	void displayRole() {
+		System.out.println("Role: Staff");
+		System.out.println("Name: " + name);
+		System.out.println("Department: " + department);
+	}
+}
