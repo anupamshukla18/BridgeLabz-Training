dfbea17a25a6494db294fc728f09c08dbac3252e
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-01 15:39:12 +0530

    [Anupam] : added person class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/problem2/Person.java
@@ -0,0 +1,13 @@
+package com.inheritance.hierarchicalinheritance.problem2;
+
+public class Person {
+	// Attributes
+	protected String name;
+	protected int age;
+
+	// Parameterized Constructor
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
