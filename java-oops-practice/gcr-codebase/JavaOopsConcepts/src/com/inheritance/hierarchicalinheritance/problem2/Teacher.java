c38624d95874f64d66b30d1c661c1a0c45926b91
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-01 15:41:15 +0530

    [Anupam] : added teacher class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/problem2/Teacher.java
@@ -0,0 +1,17 @@
+package com.inheritance.hierarchicalinheritance.problem2;
+
+public class Teacher extends Person {
+	// Additional Attribute
+	private String subject;
+
+	public Teacher(String name, int age, String subject) {
+		super(name, age);
+		this.subject = subject;
+	}
+
+	void displayRole() {
+		System.out.println("Role: Teacher");
+		System.out.println("Name: " + name);
+		System.out.println("Subject: " + subject);
+	}
+}
