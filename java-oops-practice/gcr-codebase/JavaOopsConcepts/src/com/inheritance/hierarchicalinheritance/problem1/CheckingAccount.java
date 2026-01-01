1baf61ec0a8dbd4b373b616d716eb5aed2fe6902
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-01 15:36:22 +0530

    [Anupam] : added checking account class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/problem1/CheckingAccount.java
@@ -0,0 +1,18 @@
+package com.inheritance.hierarchicalinheritance.problem1;
+
+public class CheckingAccount extends BankAccount {
+	// Additional Attribute
+	private double withdrawalLimit;
+
+	// Parameterized Constructor
+	public CheckingAccount(int accountNumber, double balance, double withdrawalLimit) {
+		super(accountNumber, balance);
+		this.withdrawalLimit = withdrawalLimit;
+	}
+
+	// Method to display account type
+	void displayAccountType() {
+		System.out.println("Account Type: Checking Account");
+		System.out.println("Withdrawal Limit: " + withdrawalLimit);
+	}
+}
