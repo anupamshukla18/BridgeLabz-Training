85f17b8e0dadacf60a4ac97bb8242a7637b3be09
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-01 15:45:54 +0530

    [Anupam] : added electric vehicle class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/problem2/ElectricVehicle.java
@@ -0,0 +1,11 @@
+package com.inheritance.hybridinheritance.problem2;
+
+public class ElectricVehicle extends Vehicle {
+	public ElectricVehicle(int maxSpeed, String model) {
+		super(maxSpeed, model);
+	}
+
+	void charge() {
+		System.out.println("Electric vehicle is charging.");
+	}
+}
