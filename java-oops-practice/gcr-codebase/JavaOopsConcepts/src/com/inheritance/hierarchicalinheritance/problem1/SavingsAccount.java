42ad3a3c80dfae793d12035ebf009f5074016819
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-01 15:37:39 +0530

    [Anupam] : added savings account class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/problem1/SavingsAccount.java
@@ -0,0 +1,18 @@
+package com.inheritance.hierarchicalinheritance.problem1;
+
+public class SavingsAccount extends BankAccount {
+	// Additional Attribute
+	private double interestRate;
+
+	// Parameterized Constructor
+	public SavingsAccount(int accountNumber, double balance, double interestRate) {
+		super(accountNumber, balance);
+		this.interestRate = interestRate;
+	}
+
+	// Method to display account type
+	void displayAccountType() {
+		System.out.println("Account Type: Savings Account");
+		System.out.println("Interest Rate: " + interestRate + "%");
+	}
+}
