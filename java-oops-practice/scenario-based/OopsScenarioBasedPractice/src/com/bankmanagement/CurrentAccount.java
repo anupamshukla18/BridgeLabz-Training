71027a34edb8fc831f60581bf61ed2b403adf1b6
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-01 11:52:42 +0530

    [Anupam] : added current account class

---
+++ java-oops-practice/scenario-based/OopsScenarioBasedPractice/src/com/bankmanagement/CurrentAccount.java
@@ -0,0 +1,11 @@
+package com.bankmanagement;
+
+class CurrentAccount extends Account {
+
+	// Constructor
+	CurrentAccount(String accountNumber, double balance) {
+		super(accountNumber, balance);
+	}
+
+	// Can add overdraft logic later if needed
+}
