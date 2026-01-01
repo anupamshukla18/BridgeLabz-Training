d583396a6780b4bb7781ea96e2b8714fd3d7dab4
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-01 15:40:49 +0530

    [Anupam] : added student class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/problem2/Student.java
@@ -0,0 +1,17 @@
+package com.inheritance.hierarchicalinheritance.problem2;
+
+public class Student extends Person {
+	// Additional Attribute
+	private String grade;
+
+	public Student(String name, int age, String grade) {
+		super(name, age);
+		this.grade = grade;
+	}
+
+	void displayRole() {
+		System.out.println("Role: Student");
+		System.out.println("Name: " + name);
+		System.out.println("Grade: " + grade);
+	}
+}
