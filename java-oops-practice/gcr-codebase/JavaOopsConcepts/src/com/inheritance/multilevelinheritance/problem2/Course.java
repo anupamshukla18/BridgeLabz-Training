032b21ca3f82ecb1de761a1000ffb3fccd1f0f75
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-01 15:32:46 +0530

    [Anupam] : added course class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelinheritance/problem2/Course.java
@@ -0,0 +1,21 @@
+package com.inheritance.multilevelinheritance.problem2;
+
+//Base Class
+public class Course {
+
+	// Attributes
+	protected String courseName;
+	protected int duration;
+
+	// Parameterized Constructor
+	public Course(String courseName, int duration) {
+		this.courseName = courseName;
+		this.duration = duration;
+	}
+
+	// Method to display course details
+	void displayCourseDetails() {
+		System.out.println("Course Name: " + courseName);
+		System.out.println("Duration: " + duration + " months");
+	}
+}
