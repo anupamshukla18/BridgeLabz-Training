7e13315e06668af0fab5ff1ee7cd66569872fe7a
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-01 15:47:09 +0530

    [Anupam] : added vehicle class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/problem2/Vehicle.java
@@ -0,0 +1,11 @@
+package com.inheritance.hybridinheritance.problem2;
+
+public class Vehicle {
+	protected int maxSpeed;
+	protected String model;
+
+	public Vehicle(int maxSpeed, String model) {
+		this.maxSpeed = maxSpeed;
+		this.model = model;
+	}
+}
