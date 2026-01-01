0cc58576b18e2de4891dd457c08963b98a4cc00c
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-01 15:34:13 +0530

    [Anupam] : added online course class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelinheritance/problem2/OnlineCourse.java
@@ -0,0 +1,21 @@
+package com.inheritance.multilevelinheritance.problem2;
+
+public class OnlineCourse extends Course {
+	// Additional Attributes
+	protected String platform;
+	protected boolean isRecorded;
+
+	// Parameterized Constructor
+	public OnlineCourse(String courseName, int duration, String platform, boolean isRecorded) {
+		super(courseName, duration);
+		this.platform = platform;
+		this.isRecorded = isRecorded;
+	}
+
+	// Method to display online course details
+	void displayCourseDetails() {
+		super.displayCourseDetails();
+		System.out.println("Platform: " + platform);
+		System.out.println("Recorded: " + isRecorded);
+	}
+}
