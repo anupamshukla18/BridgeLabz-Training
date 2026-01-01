31a40e2cce0ba4acf8e7880e8d441d0a22065a12
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-01 15:44:38 +0530

    [Anupam] : added waiter class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/problem1/Waiter.java
@@ -0,0 +1,12 @@
+package com.inheritance.hybridinheritance.problem1;
+
+public class Waiter extends Person implements Worker {
+    public Waiter(String name, int id) {
+        super(name, id);
+    }
+
+    //Override
+    public void performDuties() {
+        System.out.println("Waiter serves food to customers.");
+    }
+}
