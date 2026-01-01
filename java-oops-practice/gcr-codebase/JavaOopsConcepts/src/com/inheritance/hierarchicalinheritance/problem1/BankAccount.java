948d67e694a5fd8b70862a224be0d20a297c8d33
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-01 15:31:08 +0530

    [Anupam] : added bank account class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/problem1/BankAccount.java
@@ -0,0 +1,20 @@
+package com.inheritance.hierarchicalinheritance.problem1;
+
+public class BankAccount {
+
+	// Attributes
+	protected int accountNumber;
+	protected double balance;
+
+	// Parameterized Constructor
+	public BankAccount(int accountNumber, double balance) {
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+	}
+
+	// Method to display common account details
+	void displayAccountDetails() {
+		System.out.println("Account Number: " + accountNumber);
+		System.out.println("Balance: " + balance);
+	}
+}
