9271fb42f7dfe12060a792570ee08e3aafb053cb
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-02 13:15:43 +0530

    [Anupam] : added doctor class

---
+++ java-oops-practice/scenario-based/OopsScenarioBasedPractice/src/com/hospitalpatientmanagementsystem/Doctor.java
@@ -0,0 +1,17 @@
+package com.hospitalpatientmanagementsystem;
+
+class Doctor {
+	private int doctorId;
+	private String name;
+	private String specialization;
+
+	public Doctor(int id, String name, String specialization) {
+		this.doctorId = id;
+		this.name = name;
+		this.specialization = specialization;
+	}
+
+	public void displayInfo() {
+		System.out.println("Doctor ID: " + doctorId + ", Name: " + name + ", Specialization: " + specialization);
+	}
+}
