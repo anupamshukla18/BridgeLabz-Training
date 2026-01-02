a4c5e7cb379c714bc02621bd3fc5a0c4b564659c
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-02 11:20:09 +0530

    [Anupam] : added electronics class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/oopspillars/ecommerceplatform/Electronics.java
@@ -0,0 +1,22 @@
+package com.oopspillars.ecommerceplatform;
+
+class Electronics extends Product implements Taxable {
+	public Electronics(String id, String name, double price) {
+		super(id, name, price);
+	}
+
+	@Override
+	public double calculateDiscount() {
+		return getPrice() * 0.10; // 10% discount
+	}
+
+	@Override
+	public double calculateTax() {
+		return getPrice() * 0.18; // 18% GST
+	}
+
+	@Override
+	public String getTaxDetails() {
+		return "18% GST applied on Electronics";
+	}
+}
