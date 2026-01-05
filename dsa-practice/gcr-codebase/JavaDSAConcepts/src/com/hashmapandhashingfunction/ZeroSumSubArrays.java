4339badbcd9ff62af77ce54416a56865f866977d
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-05 14:43:30 +0530

    [Anupam] : added zero sum subarrays file

---
+++ dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/hashmapandhashingfunction/ZeroSumSubArrays.java
@@ -0,0 +1,33 @@
+package com.hashmapandhashingfunction;
+
+import java.util.*;
+
+public class ZeroSumSubArrays {
+    public static void findZeroSumSubArrays(int[] arr) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int sum = 0;
+
+        // To handle subarrays starting from index 0
+        map.put(0, new ArrayList<>());
+        map.get(0).add(-1);
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if (map.containsKey(sum)) {
+                for (int startIndex : map.get(sum)) {
+                    System.out.println("Subarray found from index "
+                            + (startIndex + 1) + " to " + i);
+                }
+            }
+
+            map.putIfAbsent(sum, new ArrayList<>());
+            map.get(sum).add(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, -7, 1, 3, -4, -2, -2};
+        findZeroSumSubArrays(arr);
+    }
+}
