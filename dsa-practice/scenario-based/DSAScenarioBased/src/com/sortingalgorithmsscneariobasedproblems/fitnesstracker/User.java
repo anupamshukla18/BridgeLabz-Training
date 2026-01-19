836d70a7f83e81f6eba6ddaaa8c92a27dd674281
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-19 14:53:49 +0530

    [Anupam] : added fitness tracker application file

---
+++ dsa-practice/scenario-based/DSAScenarioBased/src/com/sortingalgorithmsscneariobasedproblems/fitnesstracker/User.java
@@ -0,0 +1,11 @@
+package com.sortingalgorithmsscneariobasedproblems.fitnesstracker;
+
+class User {
+	String name;
+	int steps;
+
+	User(String name, int steps) {
+		this.name = name;
+		this.steps = steps;
+	}
+}
