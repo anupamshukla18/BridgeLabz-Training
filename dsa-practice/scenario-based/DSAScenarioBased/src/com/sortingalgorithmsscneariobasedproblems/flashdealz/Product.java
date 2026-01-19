1c7c880f1cc281329c4160abe387c6ca957232f6
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-19 14:55:11 +0530

    [Anupam] : added flash dealz application file

---
+++ dsa-practice/scenario-based/DSAScenarioBased/src/com/sortingalgorithmsscneariobasedproblems/flashdealz/Product.java
@@ -0,0 +1,13 @@
+package com.sortingalgorithmsscneariobasedproblems.flashdealz;
+
+class Product {
+	int id;
+	String name;
+	int discount;
+
+	Product(int id, String name, int discount) {
+		this.id = id;
+		this.name = name;
+		this.discount = discount;
+	}
+}
