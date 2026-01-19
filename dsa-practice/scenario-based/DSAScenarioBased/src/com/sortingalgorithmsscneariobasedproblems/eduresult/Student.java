06f98781116ab341b942f5d97a663f2adaf61aca
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-19 14:53:13 +0530

    [Anupam] : added edu result application file

---
+++ dsa-practice/scenario-based/DSAScenarioBased/src/com/sortingalgorithmsscneariobasedproblems/eduresult/Student.java
@@ -0,0 +1,13 @@
+package com.sortingalgorithmsscneariobasedproblems.eduresult;
+
+class Student {
+	int roll;
+	String name;
+	int marks;
+
+	Student(int roll, String name, int marks) {
+		this.roll = roll;
+		this.name = name;
+		this.marks = marks;
+	}
+}
