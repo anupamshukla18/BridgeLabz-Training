bbdd43d5f4f5d117b3b525104b68a9b5c4dd9fd3
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-23 12:21:39 +0530

    [Anupam] : added digital record system file

---
+++ dsa-practice/scenario-based/DSAScenarioBased/src/com/trees/binarysearchtree/digitalrecordsystem/StudentRecord.java
@@ -0,0 +1,12 @@
+package com.trees.binarysearchtree.digitalrecordsystem;
+
+class StudentRecord {
+	int roll;
+	String name;
+	StudentRecord left, right;
+
+	StudentRecord(int roll, String name) {
+		this.roll = roll;
+		this.name = name;
+	}
+}
