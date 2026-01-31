ca3d3c7d5487f77bdc2bf9adecab78136d3bf791
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-31 13:12:50 +0530

    [Anupam] : added multi vehicle rental file

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/definingandimplementing/multivehiclerental/Bike.java
@@ -0,0 +1,13 @@
+package com.definingandimplementing.multivehiclerental;
+
+public class Bike implements Vehicle {
+	@Override
+	public void rent() {
+		System.out.println("Bike has been rented");
+	}
+
+	@Override
+	public void returnVehicle() {
+		System.out.println("Bike has been returned");
+	}
+}
