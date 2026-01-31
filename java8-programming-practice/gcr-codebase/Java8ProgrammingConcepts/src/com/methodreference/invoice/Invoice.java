afe4e79de7b6ea0396e6763e939f21a4b6bed3c2
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-31 12:52:26 +0530

    [Anupam] : added invoice file

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/methodreference/invoice/Invoice.java
@@ -0,0 +1,16 @@
+package com.methodreference.invoice;
+
+// Invoice class
+public class Invoice {
+	int transactionId;
+
+	// Constructor that accepts transaction ID
+	public Invoice(int transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	@Override
+	public String toString() {
+		return "Invoice created for Transaction ID: " + transactionId;
+	}
+}
