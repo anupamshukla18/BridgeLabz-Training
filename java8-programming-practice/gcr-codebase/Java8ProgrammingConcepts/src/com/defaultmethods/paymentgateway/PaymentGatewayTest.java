b6cd922dd49bbc4eda26784bfa0ec046fa4b53f7
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-31 13:03:35 +0530

    [Anupam] : added payment gateway file

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/defaultmethods/paymentgateway/PaymentGatewayTest.java
@@ -0,0 +1,15 @@
+package com.defaultmethods.paymentgateway;
+
+
+public class PaymentGatewayTest {
+	public static void main(String[] args) {
+		PaymentProcessor upi = new UPIProcessor();
+		PaymentProcessor card = new CreditCardProcessor();
+
+		upi.pay(1000);
+		upi.refund(200); // default method
+
+		card.pay(2500);
+		card.refund(500); // default method
+	}
+}
