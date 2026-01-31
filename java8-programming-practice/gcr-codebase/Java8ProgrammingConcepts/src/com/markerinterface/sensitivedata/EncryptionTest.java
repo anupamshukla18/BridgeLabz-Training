00c993ba22294b2cd21ece90b9f059c92df96316
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-01-31 13:09:20 +0530

    [Anupam] : added sensitive data file

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/markerinterface/sensitivedata/EncryptionTest.java
@@ -0,0 +1,12 @@
+package com.markerinterface.sensitivedata;
+
+public class EncryptionTest {
+	public static void main(String[] args) {
+		UserCredentials creds = new UserCredentials("anuj", "password123");
+
+		UserProfile profile = new UserProfile("Anuj", "anuj@gmail.com");
+
+		EncryptionService.encrypt(creds);
+		EncryptionService.encrypt(profile);
+	}
+}
