d8f0f1927f021cdac248b91c4c0d9f8a16f65f09
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-02-05 11:45:55 +0530

    [Anupam] : added student management case study file

--- java-designpattern-practice/gcr-codebase/JavaDesignPatternConcepts/src/com/casestudy/studentlibrarymanagementsystem/UserFactory.java
+++ java-designpattern-practice/gcr-codebase/JavaDesignPatternConcepts/src/com/casestudy/studentlibrarymanagementsystem/UserFactory.java
@@ -1,17 +1,18 @@
 package com.casestudy.studentlibrarymanagementsystem;
-
 public class UserFactory {
 
-	public static User createUser(String role, String name) {
+    public static User createUser(String role, String name) {
 
-		if (role.equalsIgnoreCase("student")) {
-			return new Student(name);
-		} else if (role.equalsIgnoreCase("faculty")) {
-			return new Faculty(name);
-		} else if (role.equalsIgnoreCase("librarian")) {
-			return new Librarian(name);
-		}
+        if (role.equalsIgnoreCase("student")) {
+            return new Student(name);
+        } 
+        else if (role.equalsIgnoreCase("faculty")) {
+            return new Faculty(name);
+        } 
+        else if (role.equalsIgnoreCase("librarian")) {
+            return new Librarian(name);
+        }
 
-		return null;
-	}
+        return null;
+    }
 }
