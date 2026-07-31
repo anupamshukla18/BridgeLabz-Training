d16ee9a89f4e06c6330a35b8df246dfcc00c7648
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-07-31 16:56:51 +0530

    [Anupam] : added the updated plus minus program

--- RefresherTraining/src/day1/PlusMinus.java
+++ RefresherTraining/src/day1/PlusMinus.java
@@ -1,21 +1,35 @@
 package day1;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-class PlusMinusResult {
+public class PlusMinus {
+	public static void main(String[] args) {
+		// Scanner input
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 
-	/*
-	 * Complete the 'plusMinus' function below.
-	 *
-	 * The function accepts INTEGER_ARRAY arr as parameter.
-	 */
+		List<Integer> arr = new ArrayList<>();
 
+		// Adding the array elements
+		for (int i = 0; i < n; i++) {
+			arr.add(sc.nextInt());
+		}
+
+		// Performing the operation
+		plusMinus(arr);
+
+		sc.close();
+	}
+	
 	public static void plusMinus(List<Integer> arr) {
-		// Write your code here
 		int positive = 0;
 		int negative = 0;
 		int zeros = 0;
+		
 		int n = arr.size();
+		
 		for (int i : arr) {
 			if (i > 0) {
 				positive++;
@@ -25,28 +39,10 @@ class PlusMinusResult {
 				zeros++;
 			}
 		}
+		
+		// Printing the result
 		System.out.printf("%.6f%n", (double) positive / n);
 		System.out.printf("%.6f%n", (double) negative / n);
 		System.out.printf("%.6f%n", (double) zeros / n);
-
-	}
-}
-
-public class PlusMinus {
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-
-		// Read the array elements
-		List<Integer> arr = new ArrayList<>();
-
-		for (int i = 0; i < n; i++) {
-			arr.add(sc.nextInt());
-		}
-
-		PlusMinusResult.plusMinus(arr);
-
-		sc.close();
 	}
 }
