51d1e118fef4957a57118945a2e04c627312fd25
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-07-31 16:55:13 +0530

    [Anupam] : added the updated birthday cake candles program

--- RefresherTraining/src/day1/BirthdayCakeCandles.java
+++ RefresherTraining/src/day1/BirthdayCakeCandles.java
@@ -1,34 +1,8 @@
 package day1;
 
-import java.util.*;
-
-class BirthdayCandlesResult {
-
-	/*
-	 * Complete the 'birthdayCakeCandles' function below.
-	 *
-	 * The function is expected to return an INTEGER. The function accepts
-	 * INTEGER_ARRAY candles as parameter.
-	 */
-
-	public static int birthdayCakeCandles(List<Integer> candles) {
-		// Write your code here
-		int tallestCandle = 0;
-		for (int candle : candles) {
-			if (candle > tallestCandle)
-				tallestCandle = candle;
-			continue;
-		}
-		int countOfTallestCandles = 0;
-		for (int candle : candles) {
-			if (candle == tallestCandle)
-				countOfTallestCandles++;
-			continue;
-		}
-		return countOfTallestCandles;
-	}
-
-}
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class BirthdayCakeCandles {
 	public static void main(String[] args) {
@@ -46,11 +20,30 @@ public class BirthdayCakeCandles {
 		}
 
 		// Call the function
-		int result = BirthdayCandlesResult.birthdayCakeCandles(candles);
+		int result = birthdayCakeCandles(candles);
 
 		// Print the result
 		System.out.println(result);
 
 		sc.close();
+	}
+	public static int birthdayCakeCandles(List<Integer> candles) {
+		int tallestCandle = 0;
+		
+		for (int candle : candles) {
+			if (candle > tallestCandle)
+				tallestCandle = candle;
+			continue;
+		}
+		
+		int countOfTallestCandles = 0;
+		
+		for (int candle : candles) {
+			if (candle == tallestCandle)
+				countOfTallestCandles++;
+			continue;
+		}
+		
+		return countOfTallestCandles;
 	}
 }
