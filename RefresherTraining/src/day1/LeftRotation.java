35eb50453c5417463381739b1c0fef758d74db42
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-07-31 16:56:19 +0530

    [Anupam] : added the updated left rotation program

--- RefresherTraining/src/day1/LeftRotation.java
+++ RefresherTraining/src/day1/LeftRotation.java
@@ -2,38 +2,9 @@ package day1;
 
 import java.util.*;
 
-class LeftRotationResult {
-
-	/*
-	 * Complete the 'rotLeft' function below.
-	 *
-	 * The function is expected to return an INTEGER_ARRAY. The function accepts
-	 * following parameters: 1. INTEGER_ARRAY a 2. INTEGER d
-	 */
-
-	public static List<Integer> rotLeft(List<Integer> a, int d) {
-		// Write your code here
-		List<Integer> rotatedArray = new ArrayList<>();
-		int n = a.size();
-
-		d %= n;
-
-		for (int i = d; i < n; i++) {
-			rotatedArray.add(a.get(i));
-		}
-
-		for (int i = 0; i < d; i++) {
-			rotatedArray.add(a.get(i));
-		}
-
-		return rotatedArray;
-	}
-
-}
-
 public class LeftRotation {
 	public static void main(String[] args) {
-
+		// Scanner input
 		Scanner sc = new Scanner(System.in);
 
 		int n = sc.nextInt();
@@ -41,12 +12,16 @@ public class LeftRotation {
 
 		List<Integer> a = new ArrayList<>();
 
+		// Adding the elements in the list
 		for (int i = 0; i < n; i++) {
 			a.add(sc.nextInt());
 		}
 
-		List<Integer> result = LeftRotationResult.rotLeft(a, d);
+		// Rotating the array
+		List<Integer> result = rotLeft(a, d);
 
+
+		// Printing the list
 		for (int i = 0; i < result.size(); i++) {
 			System.out.print(result.get(i));
 
@@ -58,5 +33,24 @@ public class LeftRotation {
 		System.out.println();
 
 		sc.close();
+	}
+	
+	public static List<Integer> rotLeft(List<Integer> a, int d) {
+		List<Integer> rotatedArray = new ArrayList<>();
+		
+		int n = a.size();
+
+		d %= n;
+		
+		for (int i = d; i < n; i++) {
+			rotatedArray.add(a.get(i));
+		}
+
+		for (int i = 0; i < d; i++) {
+			rotatedArray.add(a.get(i));
+		}
+
+		// Returning the result
+		return rotatedArray;
 	}
 }
