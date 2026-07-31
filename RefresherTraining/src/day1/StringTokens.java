24c52fd6466f6b942e145ba03512f4495ae27518
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-07-31 16:53:30 +0530

    [Anupam] : added the updated strings token program

--- RefresherTraining/src/day1/StringTokens.java
+++ RefresherTraining/src/day1/StringTokens.java
@@ -1,5 +1,29 @@
 package day1;
 
-public class StringTokens {
+import java.util.Scanner;
 
+public class StringTokens {
+	public static void main(String[] args) {
+		// Scanner input
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine().trim();
+
+		// Checking if string is empty or not
+		if (str.isEmpty()) {
+			System.out.println(0);
+			return;
+		}
+
+		// Splitting the sentence into words
+		String[] arr = str.split("[^A-Za-z]+");
+
+		// Printing the result
+		System.out.println(arr.length);
+
+		for (String s : arr) {
+			System.out.println(s);
+		}
+		
+		sc.close();
+	}
 }
