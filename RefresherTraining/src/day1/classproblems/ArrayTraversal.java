eb751594aeb8358428e3d2c3a4effd9dc1b52b65
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-08-04 08:50:52 +0530

    [Anupam] : added array traversal program

---
+++ RefresherTraining/src/day1/classproblems/ArrayTraversal.java
@@ -0,0 +1,13 @@
+package day1.classproblems;
+
+public class ArrayTraversal {
+
+	public static void main(String[] args) {
+
+		int[] arr = {10, 20, 30, 40, 50};
+
+		for (int num : arr) {
+			System.out.print(num + " ");
+		}
+	}
+}
