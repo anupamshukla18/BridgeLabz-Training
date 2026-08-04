ce94bd82c3bc13324b32a78279945adabd6dc3a1
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-08-04 16:57:11 +0530

    [Anupam] : added the post order traversal program

---
+++ RefresherTraining/src/day3/hackerrankproblems/trees/PostOrderTraversal.java
@@ -0,0 +1,42 @@
+package day3.hackerrankproblems.trees;
+
+import java.util.Scanner;
+
+public class PostOrderTraversal {
+	// Inserting into the tree
+	static Node insert(Node root, int data) {
+	    if (root == null)
+	        return new Node(data);
+
+	    if (data <= root.data)
+	        root.left = insert(root.left, data);
+	    else
+	        root.right = insert(root.right, data);
+
+	    return root;
+	}
+
+	// PostOrder Traversal
+	static void postOrder(Node root) {
+	    if (root == null)
+	        return;
+
+	    postOrder(root.left);
+	    postOrder(root.right);
+	    System.out.print(root.data + " ");
+	}
+
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+
+	    int n = sc.nextInt();
+	    Node root = null;
+
+	    for (int i = 0; i < n; i++) {
+	        root = insert(root, sc.nextInt());
+	    }
+
+	    postOrder(root);
+	    sc.close();
+	}
+}
