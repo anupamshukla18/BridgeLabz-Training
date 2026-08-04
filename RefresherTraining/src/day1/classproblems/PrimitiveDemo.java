0951cef49802ad394230dba98d819e1ef18d7cf6
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-08-04 08:52:44 +0530

    [Anupam] : added primitive demo program

---
+++ RefresherTraining/src/day1/classproblems/PrimitiveDemo.java
@@ -0,0 +1,15 @@
+package day1.classproblems;
+
+public class PrimitiveDemo {
+	public static void main(String[] args) {
+		int a = 10;
+		double b = 20.5;
+		char c = 'A';
+		boolean flag = true;
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(flag);
+	}
+}
