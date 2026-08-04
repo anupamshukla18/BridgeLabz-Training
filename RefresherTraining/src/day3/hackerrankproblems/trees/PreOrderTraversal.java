988f161ddd95a71947bc8d18b92683688d5cda5d
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-08-04 16:57:39 +0530

    [Anupam] : added the pre order traversal program

---
+++ RefresherTraining/src/day3/hackerrankproblems/trees/PreOrderTraversal.java
@@ -0,0 +1,42 @@
+package day3.hackerrankproblems.trees;
+
+import java.util.Scanner;
+
+public class PreOrderTraversal {
+	// Inserting into the tree
+	static Node insert(Node root, int data) {
+		if (root == null)
+			return new Node(data);
+
+		if (data <= root.data)
+			root.left = insert(root.left, data);
+		else
+			root.right = insert(root.right, data);
+
+		return root;
+	}
+
+	// PreOrder Traversal
+	static void preOrder(Node root) {
+		if (root == null)
+			return;
+
+		System.out.print(root.data + " ");
+		preOrder(root.left);
+		preOrder(root.right);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		Node root = null;
+
+		for (int i = 0; i < n; i++) {
+			root = insert(root, sc.nextInt());
+		}
+
+		preOrder(root);
+		sc.close();
+	}
+}
