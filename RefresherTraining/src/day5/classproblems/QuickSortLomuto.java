06d7427fb64b8879ba27f7ba146a4b2b38cc8ae8
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-08-06 16:39:50 +0530

    [Anupam] : added quick sort lomuto program

---
+++ RefresherTraining/src/day5/classproblems/QuickSortLomuto.java
@@ -0,0 +1,87 @@
+package day5.classproblems;
+import java.util.Scanner;
+
+public class QuickSortLomuto {
+
+    // Recursive Quick Sort
+    static void quickSort(int[] arr, int low, int high) {
+
+        // Base case
+        if (low < high) {
+
+            // Partition the array
+            int pivotIndex = partition(arr, low, high);
+
+            // Sort left sub-array
+            quickSort(arr, low, pivotIndex - 1);
+
+            // Sort right sub-array
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    // Lomuto Partition
+    static int partition(int[] arr, int low, int high) {
+
+        // Last element is the pivot
+        int pivot = arr[high];
+
+        int i = low;
+
+        // Rearrange elements around pivot
+        for (int j = low; j < high; j++) {
+
+            if (arr[j] < pivot) {
+
+                swap(arr, i, j);
+                i++;
+            }
+        }
+
+        // Place pivot in its correct position
+        swap(arr, i, high);
+
+        return i;
+    }
+
+    // Swap two elements
+    static void swap(int[] arr, int i, int j) {
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // Print array
+    static void printArray(int[] arr) {
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        quickSort(arr, 0, n - 1);
+
+        System.out.println("Sorted Array:");
+        printArray(arr);
+
+        sc.close();
+    }
+}
