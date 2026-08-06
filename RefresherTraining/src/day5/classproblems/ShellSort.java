f88e458e2c680d2e3e0e2569979dca4be2efae1d
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-08-06 16:38:54 +0530

    [Anupam] : added shell sort program

---
+++ RefresherTraining/src/day5/classproblems/ShellSort.java
@@ -0,0 +1,63 @@
+package day5.classproblems;
+import java.util.Scanner;
+
+public class ShellSort {
+
+    // Shell Sort
+    static void shellSort(int[] arr) {
+
+        int n = arr.length;
+
+        // Reduce gap after each pass
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+
+            // Perform insertion sort for current gap
+            for (int i = gap; i < n; i++) {
+
+                int temp = arr[i];
+                int j = i;
+
+                // Shift larger elements
+                while (j >= gap && arr[j - gap] > temp) {
+                    arr[j] = arr[j - gap];
+                    j -= gap;
+                }
+
+                arr[j] = temp;
+            }
+        }
+    }
+
+    // Print array
+    static void printArray(int[] arr) {
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        shellSort(arr);
+
+        System.out.println("Sorted Array:");
+        printArray(arr);
+
+        sc.close();
+    }
+}
