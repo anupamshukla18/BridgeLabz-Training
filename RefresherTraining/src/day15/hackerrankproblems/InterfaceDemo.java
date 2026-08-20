a360f1615ef4d23ba38f6e944c00c3e62be8df84
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-08-20 15:32:38 +0530

    [Anupam] : added hackerrank problems for day 15

---
+++ RefresherTraining/src/day15/hackerrankproblems/InterfaceDemo.java
@@ -0,0 +1,30 @@
+package day15.hackerrankproblems;
+
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic {
+    public int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic {
+    public int divisor_sum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
+
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        AdvancedArithmetic a = new MyCalculator();
+        int result = a.divisor_sum(num);
+        System.out.println("I implemented: AdvancedArithmetic" + "\n" + result);
+    }
+}
---
+++ RefresherTraining/src/day15/hackerrankproblems/JavaGenerics.java
@@ -0,0 +1,23 @@
+package day15.hackerrankproblems;
+
+import java.io.*;
+import java.util.*;
+
+public class JavaGenerics {
+
+    public static <T> void printArray(T[] array) {
+
+        for (T element : array) {
+            System.out.println(element);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+
+        printArray(intArray);
+        printArray(stringArray);
+    }
+}
